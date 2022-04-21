1621bf1395cafd0355331feda3063c0850a0302e
Sudipta Sharif <sudiptasharif@gmail.com>
2022-04-20 19:36:40 -0500

    Start implementing file sync client

---
+++ src/FileClient.java
@@ -0,0 +1,13 @@
+public class FileClient {
+    private String clientType;
+    private String hostName;
+    private int portNumber;
+
+    public FileClient(String clientType, String hostName, int portNumber) {
+        this.clientType = clientType;
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+    }
+
+
+}
