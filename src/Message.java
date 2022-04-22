a1be2dcf893707f02f5fd72f3c328dcff640b6ee
Sudipta Sharif <sudiptasharif@gmail.com>
2022-04-22 02:46:06 -0500

    FSFile will be used for working with files to sync. FSUtility for utility as needed. Message for passing message between methods or object.

---
+++ src/Message.java
@@ -0,0 +1,62 @@
+/**
+ * @author sharif
+ */
+
+public class Message {
+    private final int DEFAULT_CODE = -1;
+    private final boolean DEFAULT_SUCCESS = false;
+    private String msg;
+    private int msgCode;
+    private boolean msgSuccess;
+
+    public Message(String msg) {
+        this.msg = msg;
+        this.msgCode = DEFAULT_CODE;
+        this.msgSuccess = DEFAULT_SUCCESS;
+    }
+    public Message(String msg, int msgCode, boolean msgSuccess) {
+        this.msg = msg;
+        this.msgCode = msgCode;
+        this.msgSuccess = msgSuccess;
+    }
+
+    public Message(String msg, boolean msgSuccess) {
+        this.msg = msg;
+        this.msgCode = DEFAULT_CODE;
+        this.msgSuccess = msgSuccess;
+    }
+
+    public Message(String msg, int msgCode) {
+        this.msg = msg;
+        this.msgCode = msgCode;
+        this.msgSuccess = DEFAULT_SUCCESS;
+    }
+
+    public String getMessage() {
+        return msg;
+    }
+
+    public int getMessageCode() {
+        return msgCode;
+    }
+
+    public boolean isMessageSuccess() {
+        return msgSuccess;
+    }
+
+    public void setMessageCode(int msgCode) {
+        this.msgCode = msgCode;
+    }
+
+    public void setMessageSuccess(boolean msgSuccess) {
+        this.msgSuccess = msgSuccess;
+    }
+
+    public void setMessage(String msg) {
+        this.msg = msg;
+    }
+
+    public void setMessage(String className, String methodName, String msg) {
+        this.msg = className + "@ " + methodName +": " + msg;
+    }
+}
