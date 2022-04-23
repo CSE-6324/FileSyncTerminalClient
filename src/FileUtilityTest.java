da406f10af14a743b2fa71f4db21e74a135711af
Sudipta Sharif <sudiptasharif@gmail.com>
2022-04-23 16:20:39 -0500

    Added test for getMacFiles(), and getWindowsFiles(). Note: getServerFiles() is incomplete.

--- src/FileUtilityTest.java
+++ src/FileUtilityTest.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class FileUtilityTest {
@@ -12,10 +15,22 @@ public class FileUtilityTest {
 
     @org.junit.Test
     public void getMacFiles() {
+        // I only have one file here for testing for now.
+        // Need to change this if more files are added.
+        String expectedFile = "milkyway.jpeg";
+        ArrayList<File> files = FileUtility.getMacFiles();
+        String actualFile = files.get(0).getName();
+        assertEquals(true, expectedFile.equals(actualFile));
     }
 
     @org.junit.Test
     public void getWindowsFiles() {
+        // I only have one file here for testing for now.
+        // Need to change this if more files are added.
+        String expectedFile = "alpine.jpeg";
+        ArrayList<File> files = FileUtility.getWindowsFiles();
+        String actualFile = files.get(0).getName();
+        assertEquals(true, expectedFile.equals(actualFile));
     }
 
     @org.junit.Test
